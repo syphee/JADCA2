4bf04052e441dcefbbf22cdf8c4300571f47400f
skye chew <Xxdarkness23>
2023-05-19 14:37:55 +0800

    testing

---
+++ frontend/login.js
@@ -0,0 +1,17 @@
+const switchers = [...document.querySelectorAll('.switcher')]
+
+switchers.forEach(item => {
+	item.addEventListener('click', function() {
+		switchers.forEach(item => item.parentElement.classList.remove('is-active'))
+		this.parentElement.classList.add('is-active')
+	})
+})
+
+// //
+// <!-- 
+  
+//   sites referenced:
+// https://freefrontend.com/css-login-forms/
+
+
+// -->
