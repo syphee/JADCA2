7e418cf09d45a54fbcab61dfe8d37596552831e0
syphee <jameslandicho.21@ichat.sp.edu.sg>
2023-06-15 23:53:04 +0800

    feat: Add edit user feature

--- Model ( Java classes  )/dbDAO/UserDAO.java
+++ Model ( Java classes  )/dbDAO/UserDAO.java
@@ -259,6 +259,49 @@ public class UserDAO {
 		// Step 7: Close connection
 		conn.close();
 	}
+	
+	public static void editUser(String INPUT_id,String INPUT_role) throws Exception{
+		String i = INPUT_id;
+		String USER_ROLE = "";
+		
+		int USER_ID = Integer.parseInt(i);
+		
+		if(INPUT_role.equals("admin") || INPUT_role.equals("user")){
+			USER_ROLE = INPUT_role;
+		}else {
+			throw new Exception("Input was not admin / user!");
+		}
+    	
+    	Class.forName("com.mysql.jdbc.Driver");
+
+		// Step 2: Define Connection URL
+		String connURL = "jdbc:mysql://localhost/jadca1?user=root&password="+ SQLpassword + "&serverTimezone=UTC";
+
+		// Step 3: Establish connection to URL
+		Connection conn = DriverManager.getConnection(connURL);
+		// Step 4: Create Statement object
+
+		// Call routine
+		String simpleProc = "{ call editUser(?,?) }";
+		CallableStatement cs = conn.prepareCall(simpleProc);
+
+		// insert book values
+		cs.setInt(1, USER_ID);
+		cs.setString(2, USER_ROLE);
+		
+		
+		// Step 5: Execute SQL Command
+		//String sqlStr = "SELECT * FROM member";         
+		
+		
+		
+		int affectedRows = cs.executeUpdate();
+		System.out.println("USERDAO - " + affectedRows + " row(s) deleted successfully.");
+		
+		
+		// Step 7: Close connection
+		conn.close();
+	}
 	public static void saveSession(String session_id,String INPUT_username, String INPUT_role) throws Exception {
 			
 			
